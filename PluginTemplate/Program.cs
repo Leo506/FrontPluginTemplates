4f409d3edb739be6f5d6fe04144b8d4d07c2c102
Artem Levin <alevin@arbus.biz>
2025-04-24 15:19:34 +0500

    init

---
+++ PluginTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using Resto.Front.Api;
+using Resto.Front.Api.Attributes;
+namespace PluginTemplate;
+
+[PluginLicenseModuleId(LICENSE_MODULE_ID)]
+public class Program : IFrontPlugin
+{
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
